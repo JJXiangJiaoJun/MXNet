5a8f3da272f47c8f1a838e02b9613924c42ac608
JJXiangJiaoJun <591495195@qq.com>
2019-03-21 19:47:26 +0800

    style transfer

--- chapter6_computer_vision/script.py
+++ chapter6_computer_vision/script.py
@@ -25,7 +25,7 @@ def load_data_pikachu(batch_size,edge_size=256):
                                   path_imgrec=os.path.join(data_dir, 'val.rec'), batch_size=batch_size,
                                  data_shape=(3, edge_size, edge_size), shuffle=False)
     return train_iter,val_iter
-
+gloss.L1Loss
 
 #获取皮卡丘数据
 batch_size = 64
